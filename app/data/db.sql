9cf63f2b94862b2f3583ddf2b4b17c00c90d1e40
Shiva <shivably@gmail.com>
2021-03-27 23:38:55 +0530

    expense tables added

--- app/data/db.sql
+++ app/data/db.sql
@@ -39,16 +39,35 @@ CREATE TABLE IF NOT EXISTS Income
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     donor_name TEXT DEFAULT NULL,
     donor_contact TEXT DEFAULT NULL,
-    member_id INTEGER NOT NULL, 
+    member_id INTEGER, 
     income_type_id INTEGER NOT NULL,
     kovil_id INTEGER NOT NULL,
-    quantity_value INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
+    quantity_value INTEGER,
+    quantity INTEGER,
     value INTEGER NOT NULL,
     description TEXT,
     donation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(member_id) REFERENCES Members(id),
     FOREIGN KEY(income_type_id) REFERENCES IncomeType(id),
     FOREIGN KEY(kovil_id) REFERENCES Kovil(id)
+);
 
+CREATE TABLE IF NOT EXISTS ExpenseType
+( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT NOT NULL, 
+    description TEXT, 
+    active BOOLEAN DEFAULT 1,
+    dor DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS Expense
+( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    type_id INTEGER NOT NULL, 
+    receipt TEXT DEFAULT NULL, 
+    value INTEGER NOT NULL,
+    description TEXT, 
+    dor DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(type_id) REFERENCES ExpenseType(id)
 );
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,28 +1,13 @@
-/*const getRoutes = require('./product-get-route');
-const postRoutes = require('./product-post-route');
-const putRoutes = require('./product-put-route');
-const deleteRoutes = require('./product-delete-route');*/
 const loadDatabase = require('../data/setup-database');
-
 const members = require('./member');
 const income_type = require('./income_type');
 const donation = require('./donation');
 const reports = require('./reports');
 
-
-
 module.exports = function (app, db) {
-
   // create database in case it was not created yeat, 
   // or update in case of migrations
   loadDatabase(db);
-
-  // start routes
-  /*getRoutes(app, db);
-  postRoutes(app, db);
-  putRoutes(app, db);
-  deleteRoutes(app, db);*/
-
   members(app, db);
   income_type(app, db);
   donation(app, db);
