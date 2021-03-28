446a3a1f7ea0b9288f5fd2f4f585e14c4968c0f5
Shiva <shivably@gmail.com>
2021-03-28 10:53:32 +0530

    minor fixes

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -1,4 +1,4 @@
-module.exports = function(app, db) {
+module.exports = function (app, db) {
   app.get('/api/reports/:member_id/:income_type_id', (req, res) => {
     var member_id = req.params.member_id;
     var income_type_id = req.params.income_type_id;
@@ -6,25 +6,37 @@ module.exports = function(app, db) {
     processData(res, "SELECT * FROM Income where member_id = " + member_id + " and income_type_id = " + income_type_id);
   });
 
-  function processData(res, sql){
-    db.serialize(function() {
-      db.all(sql, 
-        function(err, rows) {
-          if(err){
+  app.get('/api/reports/id/', (req, res) => {
+    var id = req.params.id;
+    console.dir(req)
+    processData(res, "SELECT * FROM Income where id = " + id);
+  });
+
+  app.post('/api/reports/', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    var data = req.body;
+    processData(res, "SELECT * FROM Income where donor_name like %" + donor_name.name + "%");
+  });
+
+  function processData(res, sql) {
+    db.serialize(function () {
+      db.all(sql,
+        function (err, rows) {
+          if (err) {
             console.error(err);
             res.status(500).send(err);
           }
           else
             sendData(res, rows, err);
-      });
+        });
     });
   }
 
-  function sendData(res, data, err){
-    res.setHeader("Access-Control-Allow-Origin","*");
-    if(data[0])
+  function sendData(res, data, err) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    if (data[0])
       res.send(data);
-    else{
+    else {
       res.status(404).send("Income not found");
     }
   }
