2a375fd41e2ab295fd5f5568b56aba5702816fe6
senthil <shivably@gmail.com>
2021-04-25 15:49:39 +0530

    translation for english

---
+++ app/assets/config/language.js
@@ -0,0 +1,263 @@
+var lang_dict = {
+    //////////////////// add-donation-ayyan.html/////////////////////////
+    "AyyanKovilDonation": {
+        "ta": "அயன் கோயில் நன்கொடை",
+        "en": "Ayyan Kovil Donation"
+    },
+    "Member": {
+        "ta": "உறுப்பினர்",
+        "en": "Member"
+    },
+    "SelectMembers": {
+        "en": "Select Members",
+        "ta": "hey"
+    },
+    "Name": {
+        "en": "Name",
+        "ta": "hey"
+    },
+    "Contact": {
+        "en": "Contact",
+        "ta": "hey"
+    },
+    "KodaiVari": {
+        "en": "Kodai Vari",
+        "ta": "hey"
+    },
+    "Unitvalue": {
+        "en": "Unit value",
+        "ta": "hai"
+    },
+    "SeethanaVari": {
+        "en": "Seethana Vari",
+        "ta": "hey"
+    },
+    "NankudaiVaravu": {
+        "en": "Nankudai Varavu",
+        "ta": "hey"
+    },
+    "UndiyalVaravu": {
+        "en": "Undiyal Varavu",
+        "ta": ""
+    },
+    " NemisaVaravu": {
+        "en": " Nemisa Varavu",
+        "ta": ""
+    },
+    "ItharaVaravu": {
+        "en": "Ithara Varavu",
+        "ta": ""
+    },
+    "TotalAmount": {
+        "en": "Total Amount",
+        "ta": ""
+    },
+    ////////////////// add_expense_type.html/////////////////////////
+    "AddExpenseType": {
+        "en": "Add Expense Type",
+        "ta": ""
+    },
+    "Description": {
+        "en": "Description",
+        "ta": ""
+    },
+    ////////////////////////add_expense.html////////////////////////////
+    "AddExpense": {
+        "en": "Add Expense",
+        "ta": ""
+    },
+    "Date:": {
+        "en": "Date:",
+        "ta": ""
+    },
+    "Type": {
+        "en": "Type",
+        "ta": ""
+    },
+    "ExpenseType": {
+        "en": "Expense Type",
+        "ta": ""
+    },
+    "Receipt": {
+        "en": "Receipt",
+        "ta": ""
+    },
+    "Amount": {
+        "en": "Amount",
+        "ta": ""
+    },
+    ////////////////////add_income_type/////////////
+    "AddDonationType": {
+        "en": "Add Donation Type",
+        "ta": ""
+    },
+    "Kovil": {
+        "en": "Kovil",
+        "ta": ""
+    },
+    "Value": {
+        "en": "Value",
+        "ta": ""
+    },
+    ////////////////add-donation-parvathiamman.html////////////
+    "ParvathiammanKovilDonation": {
+        "en": "Parvathiamman Kovil Donation",
+        "ta": ""
+    },
+    ///////////////////////add-donation.html/////////////////
+    "Donation": {
+        "en": "Donation",
+        "ta": ""
+    },
+    ////////////////////add-member.html///////////////////
+    "AddMember": {
+        "en": "Add Member",
+        "ta": ""
+    },
+    "Address": {
+        "en": "Address",
+        "ta": ""
+    },
+    "PermanentAddress": {
+        "en": "Permanent Address",
+        "ta": ""
+    },
+    "Phone": {
+        "en": "Phone",
+        "ta": ""
+    },
+    "Email": {
+        "en": "Email",
+        "ta": ""
+    },
+    "Aadhar": {
+        "en": "Aadhar",
+        "ta": ""
+    },
+    "DateOfBirth": {
+        "en": "Date Of Birth",
+        "ta": ""
+    },
+    //////////////////////////balance.html////////////////////////////
+    "BalanceSheet": {
+        "en": "Balance Sheet",
+        "ta": " check"
+    },
+    "AccountBalance:": {
+        "en": "Account Balance",
+        "ta": ""
+    },
+    "Expense:": {
+        "en": "Expense",
+        "ta": ""
+    },
+    "Balance:": {
+        "en": "Balance:",
+        "ta": ""
+    },
+    //////////////////////////donations.html////////////////////////
+    "ID": {
+        "en": "ID",
+        "ta": ""
+    },
+    "QuantityValue": {
+        "en": "Quantity Value",
+        "ta": ""
+    },
+    "Quantity": {
+        "en": "Quantity",
+        "ta": ""
+    },
+    "AddDonation": {
+        "en": "Add Donation",
+        "ta": ""
+    },
+    //////////////////////edit_expense.html/////////////////////////
+    "EditExpense": {
+        "en": "Edit Expense",
+        "ta": ""
+   },
+   ////////////////////////edit-member.html/////////////////////////
+   "EditMember":{
+       "en": "Edit Member",
+       "ta": ""
+   },
+   /////////////////////////expense_report.html/////////////////////
+   "ExpenseReport":{
+       "en": "Expense Report",
+       "ta": ""
+   },
+   "From:":{
+       "en": "From:",
+       "ta": ""
+   },
+   "To:":{
+       "en": "To:",
+       "ta": ""
+   },
+   "Type:":{
+       "en": "Type:",
+       "ta": ""
+   },
+   "Email:":{
+       "en": "Email:",
+       "ta": ""
+   },
+   "Date":{
+       "en": "Date",
+       "ta": ""
+   },
+   ////////////////////income_type.html//////////////////
+   "AddIncomeType":{
+       "en": "Add Income Type",
+       "ta": ""
+   },
+   ///////////////////kovil.html//////////////////////////
+   "AddKovil":{
+       "en": "Add Kovil",
+       "ta": ""
+   },
+   /////////////////////receipt.html////////////////////////
+   "ReceiptNumber:":{
+       "en": "Receipt Number:",
+       "ta": ""
+   },
+   "UnitPrice":{
+       "en": "Unit Price",
+       "ta": ""
+   },
+   "Total":{
+       "en": "Total",
+       "ta": ""
+   },
+   "Thankyouforyoursupport":{
+       "en": "Thank you for your support",
+       "ta": ""
+   },
+   "Print":{
+       "en": "Print",
+       "ta": ""
+   },
+   /////////////////////////reports.html/////////////////
+   "ReceiptNumber":{
+       "en": "Receipt Number",
+       "ta": ""
+   },
+   "From":{
+       "en": "From",
+       "ta": ""
+   },
+   ///////////////////////navigation.html///////////////////
+   "Reports":{
+       "en": "Reports",
+       "ta": "check"
+   },
+   "Members":{
+       "en": "Members",
+       "ta": " c"
+   },
+   "Expenses":{
+       "en": "Expenses",
+       "ta": "cc"
+   },
+}
