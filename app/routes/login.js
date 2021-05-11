a3c2ea5ca13bd08cc9e9914175da441e12fd0166
Senthil <senthilprakash2020@gmail.com>
2021-05-11 11:15:16 +0530

    Done login page validation and logoutnav

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,34 +1,32 @@
 module.exports = function (app, db) {
-    // Load products by ID: http://localhost:4300/api/income_type/id/$id
-    // example: http://localhost:4300/api/income_type/id/15
-    app.post('/api/login/', (req, res) => {
-        var credential = req.body;
-        autheticateUser(credential, res, db);
-    });
-  };
+  app.post('/api/login/', (req, res) => {
+    var credential = req.body;
+    return autheticateUser(credential, res, db);
+  });
+};
 
-  function autheticateUser(credential, res, db){
-    var username = credential.username;
-    var password = credential.password;
-  
-    if(!username){
-      res.status(200).send({response_action: 'redirect', url:'/login', msg: "Login Failed!"});
-    }
-    else{
-        var sql = `select username,isAdmin from Users where username = ? and password = ?;`;
-        var values = [username, password];
+function autheticateUser(credential, res, db) {
+  var username = credential.username;
+  var password = credential.password;
 
-        db.get(sql, values, (err, row) => {
-            if (err) {
-              return console.error(err.message);
-            }
-            if (row){
-              res.cookie('sss-session', 1000, { maxAge: 900000, httpOnly: true });
-              res.status(200).send({response_action: 'redirect', url:'/add-donation-parvathiamman', msg: "Login Successfull!"})
-            }else{
-              res.status(200).send({response_action: 'redirect', url:'/login', msg: "Login Failed!"});
-            }
-          });
-
-    }
+  if (!username && !password) {
+    return { response_action: 'redirect', url: '/login', msg: "Login Failed!" };
   }
+  else {
+    var sql = `select username, isAdmin, language from Users where username = ? and password = ?;`;
+    var values = [username, password];
+
+    db.get(sql, values, (err, row) => {
+      if (err) {
+        return { response_action: 'redirect', url: '/login', msg: "Login Failed!" };
+      }
+      if (row) {
+        res.cookie('sss-session', username + ":" + password, { maxAge: 900000, httpOnly: true });
+        //res.status(200).send({ response_action: 'redirect', url: '/add-donation-parvathiamman', msg: "Login Successfull!" })
+        return { response_action: 'redirect', url: '/add-donation-parvathiamman', msg: "Login Successfull!" };
+      } else {
+        return { response_action: 'redirect', url: '/login', msg: "Login Failed!" };
+      }
+    });
+  }
+}
--- server.js
+++ server.js
@@ -1,151 +1,201 @@
 const express = require('express');
 var cookieParser = require('cookie-parser')
+var sqlite3 = require('sqlite3');
+var bodyParser = require("body-parser");
+var backup = require('./app/routes/backup');
+
+
+var db = new sqlite3.Database('app/data/sqlitedb');
 const app = express();
 const port = 80;
 const ip = '0.0.0.0'
-
-var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('app/data/sqlitedb');
-var bodyParser = require("body-parser");
+require('./app/routes')(app, db);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
-require('./app/routes')(app, db);
-var backup = require('./app/routes/backup');
-const { schedule } = require('node-cron');
+
 
 var notLoggedIn = 0;
 
-app.get('/', (req, res) => {
+function userLoggedIn(req, res) {
+
+    if (req.cookies['sss-session'] == null || req.cookies['sss-session'] == "") {
+        notLoggedIn = 1
+        return
+    }
     console.log('Cookies: ', req.cookies['sss-session'])
-    if(notLoggedIn) {
+    var credential = req.cookies['sss-session'].split(':');
+    var username = credential[0];
+    var password = credential[1];
+    console.log(username)
+    console.log(password)
+    if (!username && !password) {
+        notLoggedIn = 1
+    } else {
+        var sql = `select username, isAdmin, language from Users where username = ? and password = ?;`;
+        var values = [username, password];
+
+        db.get(sql, values, (err, row) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            if (row) {
+                notLoggedIn = 0
+            } else {
+                notLoggedIn = 1
+            }
+        });
+    }
+}
+
+app.get('/', (req, res) => {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
-    }else{
+    } else {
         res.sendFile('./app/assets/html/add-donation-parvathiamman.html', { root: __dirname });
     }
 });
 
 app.get('/members', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
-    }else{
+    } else {
         res.sendFile('./app/assets/html/members.html', { root: __dirname });
     }
 });
 
 app.get('/add-member', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/add-member.html', { root: __dirname });
 });
 
 app.get('/kovil', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/kovil.html', { root: __dirname });
 });
 
 app.get('/add_kovil', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/add_kovil.html', { root: __dirname });
 });
 
 app.get('/income_type', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/income_type.html', { root: __dirname });
 });
 app.get('/add_income_type', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/add_income_type.html', { root: __dirname });
 });
 
 app.get('/add-donation-ayyan', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/add-donation-ayyan.html', { root: __dirname });
 });
 
 app.get('/add-donation-parvathiamman', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/add-donation-parvathiamman.html', { root: __dirname });
 });
 
 app.get('/add-donation', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/add-donation.html', { root: __dirname });
 });
 
 app.get('/expenses', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/expenses.html', { root: __dirname });
 });
 
 app.get('/add_expense_type', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/add_expense_type.html', { root: __dirname });
 });
 
 app.get('/add_expense', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/add_expense.html', { root: __dirname });
 });
 
 app.get('/reports', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/reports.html', { root: __dirname });
 });
 
 app.get('/receipt', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/receipt.html', { root: __dirname });
 });
 app.get('/edit-member', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/edit-member.html', { root: __dirname });
 });
 
 app.get('/edit_expense', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/edit_expense.html', { root: __dirname });
 });
 
 app.get('/expense_report', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/expense_report.html', { root: __dirname });
 });
 
 app.get('/balance', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
     }
     res.sendFile('./app/assets/html/balance.html', { root: __dirname });
@@ -166,15 +216,48 @@ app.get('/config/language.js', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile('./app/assets/html/login.html', { root: __dirname });
 });
+
+app.post('/login', (req, res) => {
+    var credential = req.body;
+    console.dir(credential)
+    var username = credential.username;
+    var password = credential.password;
+
+    if (!username && !password) {
+        res.status(200).send({ response_action: 'redirect', url: '/login', msg: "Login Failed!" });
+    }
+    else {
+        var sql = `select username, isAdmin, language from Users where username = ? and password = ?;`;
+        var values = [username, password];
+
+        db.get(sql, values, (err, row) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            if (row) {
+                res.cookie('sss-session', username + ":" + password, { maxAge: 900000, httpOnly: true });
+                //res.status(200).send({ response_action: 'redirect', url: '/add-donation-parvathiamman', msg: "Login Successfull!" })
+                res.redirect('/add-donation-parvathiamman?msg=Login Successfull!');
+            } else {
+                res.status(200).send({ response_action: 'redirect', url: '/login', msg: "Login Failed!" });
+            }
+        });
+    }
+});
+
 app.get('/backup', (req, res) => {
-    if(notLoggedIn) {
+    userLoggedIn(req, res)
+    if (notLoggedIn) {
         res.redirect('/login');
-    }else{
+    } else {
         res.sendFile('./app/assets/html/backup.html', { root: __dirname });
     }
 });
 
-
+app.get('/logout', (req, res) => {
+    res.cookie('sss-session', "", { maxAge: Date.now(), httpOnly: true });
+    res.redirect('/login?msg=Logged Out');
+});
 
 app.listen(port, ip, () => {
     console.log('Backend NodeJS live on ' + port);
